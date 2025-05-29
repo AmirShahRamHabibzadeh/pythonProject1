4d77662313fd6dc7ab542fae4ace102c3af4013e
amir_shahram_habibzadeh <shabibzadeh11@gmail.com>
2025-05-29 17:49:31 +0330

    laptop_manager کار کلاسی

---
+++ laptop/laptop.py
@@ -0,0 +1,13 @@
+
+
+class Laptop:
+    def __init__(self, brand, ram, cpu, screensize, man_date, price):
+        self.brand = brand
+        self.ram = ram
+        self.cpu = cpu
+        self.screensize = screensize
+        self.man_date = man_date
+        self.price = price
+
+    def save_laptop(self):
+        print(f"{self.brand}-{self.ram}-{self.cpu}-{self.screensize}-{self.man_date}-{self.price}saved")
